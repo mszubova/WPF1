379889bf6075362fc5e18298e72e78cb00637a56
Maria Zubova <73553427+mszubova@users.noreply.github.com>
2021-10-12 03:57:45 +0500

    Add files via upload

---
+++ _8Ball/_8Ball/AnswerGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Ball
+{
+    public class AnswerGenerator
+    {
+        private static string[] answers = new string[5] { "Возможно", "Не возможно", "Да", "Нет", "Сомнительно" };
+        private static string answer;
+        private static Random random = new Random();
+        public static string GetRandomAnswer()
+        {
+            answer = answers[random.Next(0, answers.Length)];
+            return answer;
+        }
+    }
+}
---
+++ _8Ball/_8Ball/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8Ball
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Answer_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            txtAnswer.Text = AnswerGenerator.GetRandomAnswer();
+            this.Cursor = null;
+        }
+        private void txtQuestion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}
